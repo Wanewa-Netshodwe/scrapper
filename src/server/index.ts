ac9b43d4ffbb74dd259f53e8b07c0e86759c4284
Wanewa-Netshodwe <waneexw@gmail.com>
2025-10-27 18:40:48 +0200

    changed port number

--- src/server/index.ts
+++ src/server/index.ts
@@ -9,10 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 app.use("/api/scrape", scrapeRoute);
 
-app.listen(port, () => {
+app.listen(4500, () => {
   console.log(`server is running on port ${port}`);
 });
